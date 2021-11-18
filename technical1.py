a29ceb8d10db47d4f6baccbefba8a7c83322abb6
Alex McIntosh <alex.mcintosh@octoenergy.com>
2021-11-18 15:04:54 +0000

    Re pushed the file

--- technical1.py
+++ technical1.py
@@ -1,10 +1,25 @@
 # provide code that iterates in multiples of 7 up until 100, then in multiples of 8 up to 200, then multiples of 9 up to 300.
 def itermultiples():
-    for i in range(1, 101, 7):
+    for i in range(1, 100, 7):
         print(i)
-    for i in range(1, 201, 8):
+    for i in range(1, 200, 8):
         print(i)
-    for i in range(1, 301, 9):
+    for i in range(1, 300, 9):
         print(i)
 
 itermultiples()
+
+# code that prints multiples of 7 until 100, then multiples of 8 up to 200, then multiples of 9 up to 300.
+def itermultiples2():
+    x = 0
+    while x < 100:
+        print(x)
+        x += 7
+    while x < 200: 
+        print(x)
+        x += 8
+    while x < 300:
+        print(x)
+        x += 9
+        
+itermultiples2()
